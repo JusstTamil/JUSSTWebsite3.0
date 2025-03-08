b9935ab7273109cc21d1a3c353b2b07d4201dfe9
JusstTamil <128580189+JusstTamil@users.noreply.github.com>
2025-03-08 22:18:24 +0530

    Update Contact.jsx
    Exchanged email address with random string given by formSubmit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -17,7 +17,7 @@ const Contact = () => {
 export const ContactForm = () => {
   return (
     <React.Fragment>
-      <form className="md:w-1/2 w-4/5 h-full flex flex-col justify-around items-center" action="https://formsubmit.co/jusst2023@gmail.com" method="POST">
+      <form className="md:w-1/2 w-4/5 h-full flex flex-col justify-around items-center" action="https://formsubmit.co/fb66386f161a177f3458cb01bfc3b521" method="POST">
         {/* <input type="hidden" name="_next" value="/thanks" /> */}
         <div className="contact-form-element">
           <label htmlFor="Name">
